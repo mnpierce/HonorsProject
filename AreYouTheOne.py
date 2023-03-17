29d8a1fc89d8ebdf74917cfa46e5a944a16b825b
Matt Pierce <mnpierce12@gmail.com>
2023-03-17 01:20:27 -0400

    Working

--- AreYouTheOne.py
+++ AreYouTheOne.py
@@ -1,7 +1,7 @@
 # Import random module
 import random
 
-names = ['Sherri','Gunther','Clinton','Karl','Douglas','Suzanne','Jules','Jerold','Raymond','Annette','Alina','Gabriela','Maria','Placida','Mariano','Vincent']
+names = []
 
 class Contestant:
     '''Set up contestant objects'''
@@ -14,67 +14,127 @@ class Contestant:
         '''Return string representation of object'''
         return f'{self.name}'
 
-
 class Matches:
-    '''Create perfect matches'''
+    '''Create matches (used for perfect matches and guessing matches)'''
 
-    def __init__(self, names):
+    def __init__(self, contestants):
         '''Initialize contestant list and perfect matches'''
-
-        contestants = []
-
-        for name in names:
-            contestants.append(Contestant(name))
         
         self.contestants = contestants
 
         random_matches = []
         temp_contestants = self.contestants.copy()
         
-        for i in range(8):                                                  # Run loop 8 times to account for 8 pairs (given 16 contestants)
+        for i in range(len(self.contestants)//2):                                                  # Run loop 8 times to account for 8 pairs (given 16 contestants)
             random_pair_list = random.choices(temp_contestants,k=2)         # Get two random names from list
             
             while random_pair_list[0] == random_pair_list[1]:               # Establish while loop to handle if two names are the same person
                 random_pair_list = random.choices(temp_contestants,k=2)     
             
-            random_tuple = (random_pair_list[0],random_pair_list[1])        # Create a tuple to hold match
-            random_matches.append(random_tuple)                             # Add match tuple to list of matches
+            random_set = set((random_pair_list[0],random_pair_list[1]) )       # Create a tuple to hold match
+            random_matches.append(random_set)                             # Add match tuple to list of matches
             
             temp_contestants.remove(random_pair_list[0])                    # Remove the contestants that have been matched
             temp_contestants.remove(random_pair_list[1])
 
-        self.perfect_matches = random_matches
+        self.matches = random_matches
 
 class Simulation:
     '''Simulates game'''
 
-    def __init__(self):
+    def __init__(self,names):
         '''Initialize game'''
-
-        self.game = Matches(names)
+        contestants = []
+        for name in names:
+            contestants.append(Contestant(name))
+        self.contestants = contestants
+        self.game = Matches(self.contestants)
+        self.matches_found = []
+        self.bad_matches = []
 
     def guess_matches(self):
         '''Randomize guesses for perfect couples'''
-        pass
+        # self.guess = Matches(self.contestants)
+        # check = True
+        # while check:
+        #     check = any(match in self.bad_matches for match in self.guess.matches)
+        #     self.guess = Matches(self.contestants)
+        random_matches = []
+        temp_contestants = self.contestants.copy()
+        
+        for i in range(len(self.contestants)//2):                                                  # Run loop 8 times to account for 8 pairs (given 16 contestants)
+            random_pair_list = random.choices(temp_contestants,k=2)         # Get two random names from list
+            
+            while random_pair_list[0] == random_pair_list[1]:               # Establish while loop to handle if two names are the same person
+                random_pair_list = random.choices(temp_contestants,k=2)
+            while random_pair_list in self.bad_matches:
+                random_pair_list = random.choices(temp_contestants,k=2)
+            
+            random_set = set((random_pair_list[0],random_pair_list[1]) )       # Create a tuple to hold match
+            random_matches.append(random_set)                             # Add match tuple to list of matches
+            
+            temp_contestants.remove(random_pair_list[0])                    # Remove the contestants that have been matched
+            temp_contestants.remove(random_pair_list[1])
+
+        self.guess = random_matches
+        return self.guess
     
     def analyze_matches(self):
         '''Return number of correct perfect couples'''
-        pass
+        self.count = len(self.matches_found)
+        # for match in self.guess.matches:
+        for match in self.guess:
+            if match in self.game.matches: self.count += 1
+
+        if self.count == len(self.matches_found):
+            # for match in self.guess.matches:
+            for match in self.guess:
+                self.bad_matches.append(match)
+
+        return f'{self.count} couples are correct!'
 
     def truth_booth(self):
         '''
         Random couple is sent to "truth booth"
-        Returns True/False (True if perfect match)
+        Returns string indicating if match is correct
         '''
-        pass
+        # self.random_couple = random.choice(self.guess.matches)
+        self.random_couple = random.choice(self.guess)
+        if self.random_couple in self.game.matches:
+            for contestant in self.random_couple:
+                self.contestants.remove(contestant)
+
+            self.matches_found.append(self.random_couple)
+            return f'{list(self.random_couple)[0]} and {list(self.random_couple)[1]} are a perfect match!'
+        else: 
+            if self.random_couple not in self.bad_matches: self.bad_matches.append(self.random_couple)
+            return f'{list(self.random_couple)[0]} and {list(self.random_couple)[1]} are NOT a match!'
 
 def start_sim():
-
-    sim = Simulation()
-    f1 = f'Contestants: {sim.game.contestants}'
-    print(f1)
-    f2 = f'Perfect Matches: {sim.game.perfect_matches}'
-    print(f2)
-
+    global names
+    names = ['Sherri','Gunther','Clinton','Karl','Douglas','Suzanne','Jules','Jerold','Raymond','Annette','Alina','Gabriela','Maria','Placida','Mariano','Vincent']
+    # contestants = ContestantList(names).output()
+    sim = Simulation(names)
+    print(f'Contestants: {sim.game.contestants}')
+    finish = False
+    week = 0
+    while not finish:
+        week += 1
+        print(f'\nWeek {week}\n----------------------')
+        sim.guess_matches()
+        analyze = sim.analyze_matches()
+        print(analyze)
+        if sim.count == len(sim.game.matches):
+            print('All matches found!')
+            print(f'Perfect matches: {sim.game.matches}')
+            return
+        if sim.count != len(sim.matches_found):
+            truth = sim.truth_booth()
+            print(f'{list(sim.random_couple)[0]} and {list(sim.random_couple)[1]} have been sent to the truth booth!')
+            print(truth)
+        else: print('No new correct matches')
+        if sim.matches_found != []:
+            print(f'Perfect matches found: {sim.matches_found}')
+            
 
 start_sim()
