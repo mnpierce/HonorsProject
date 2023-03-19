0128a7c4826cca7436de27f348a19d2cfb2cbb45
Matt Pierce <mnpierce12@gmail.com>
2023-03-19 00:58:24 -0400

    full comments

--- AreYouTheOne.py
+++ AreYouTheOne.py
@@ -1,7 +1,6 @@
-# Import random module
+# Import random and math modules
 import random
-
-names = []
+from math import factorial as fact  # import factorial from math
 
 class Contestant:
     '''Set up contestant objects'''
@@ -15,84 +14,130 @@ class Contestant:
         return f'{self.name}'
 
 class Matches:
-    '''Create matches (used for perfect matches and guessing matches)'''
+    '''
+    Create matches (used for perfect matches and guessing matches)
+    This is done with random generation to best simulate how matches would be formed in a real reality TV show game (there should be no systematic approach to predict matches)
+    '''
+
     def __init__(self, contestants,bad_matches):
         '''Initialize contestant list and perfect matches'''
+
+        # initialize bad matches with passed in list
         self.bad_matches = bad_matches
+
+        # use while loop to add functionality to restart the whole list if there is no way to make a valid list during any given attempt
+        # this would happen if the only possible matches remaining are already in the bad matches list
         keepgoing = True
         while keepgoing:
-            random_matches = []
-            temp_contestants = contestants.copy()
-            for i in range(len(contestants)//2):                                                  # Run loop 8 times to account for 8 pairs (given 16 contestants)
-                random_pair_set = set(random.choices(temp_contestants,k=2) )        # Get two random names from list
-                
-                while len(random_pair_set) == 1 or random_pair_set in self.bad_matches:               # Establish while loop to handle if two names are the same person
-                    random_pair_set = set(random.choices(temp_contestants,k=2))
-                    if len(temp_contestants) == 2 and random_pair_set in self.bad_matches:
-                        break
-                
-                if len(temp_contestants) == 2 and random_pair_set in self.bad_matches:
-                    break
 
-                # random_set = set((list(random_pair_set)[0],list(random_pair_set)[1]) )       # Create a tuple to hold match
-                random_matches.append(random_pair_set)                             # Add match tuple to list of matches
-                
-                temp_contestants.remove(list(random_pair_set)[0])                    # Remove the contestants that have been matched
+            # initialize matches list to add to
+            random_matches = []
+
+            # create temporary contestants list to modify
+            temp_contestants = contestants.copy()
+
+            # use for loop that runs the exact amount of times needed to create the right amount of pairs
+            for i in range(len(contestants)//2):
+
+                # get two random contestants from the contestants list
+                random_pair_set = set(random.choices(temp_contestants,k=2))
+
+                # calculate the number possible pair permutations given the number of contestants
+                n = len(temp_contestants)
+                r = 2
+                max_loops = int(fact(n) / (fact(n-r)*fact(r)))
+
+                # use while loop to handle bad generated pairs
+                # cases:
+                #   1) The same contestant is randomly chosen e.g. {Mariano}
+                #   2) The pair is already in the list of bad matches
+
+                count = 0   # counter
+
+                while len(random_pair_set) == 1 or random_pair_set in self.bad_matches:
+                    count += 1  # increment counter
+
+                    # re-generate the pair
+                    random_pair_set = set(random.choices(temp_contestants,k=2))
+
+                    # if re-generation attempts exceed the maximum number of possibilities, break out of loop to restart entire list
+                    if  count >= max_loops:
+
+                        # if still a bad pair on last final attempt, break out of loop, otherwise will continue
+                        if random_pair_set in self.bad_matches:
+                            break
+
+                # same as previous two if statements to break out of the for loop as well
+                if count >= max_loops:
+                    if random_pair_set in self.bad_matches:
+                        break
+
+                # add the pair to the list of matches
+                random_matches.append(random_pair_set)
+
+                # remove the contestants that have been matched from the temporary list so they are not used again
+                temp_contestants.remove(list(random_pair_set)[0])
                 temp_contestants.remove(list(random_pair_set)[1])
 
+                # if no contestants remain, the full list is generated and the while loop can terminate
                 if len(temp_contestants) == 0:
                     keepgoing = False
 
+        # initialize matches to be used in composition in other classes
         self.matches = random_matches
-        
+
 class Simulation:
     '''Simulates game'''
 
     def __init__(self,names):
         '''Initialize game'''
+        
+        # set up empty list of contestants to append to
         contestants = []
+        
+        # create Contestant objects
         for name in names:
             contestants.append(Contestant(name))
-        self.contestants = contestants
-        self.bad_matches = []
-        self.game = Matches(self.contestants,self.bad_matches)
-        self.matches_found = []
         
+        # initialize list of Contestant objects
+        self.contestants = contestants
+        
+        # initialize list of bad matches that will be updated in .analyze_matches() and .truth_booth()
+        self.bad_matches = []
+        
+        # create instance of Matches class that holds the perfect matches that will be used the goal of the game
+        self.game = Matches(self.contestants,self.bad_matches)
+
+        # initialize list of confirmed matches that will be updated in .truth_booth()
+        self.matches_found = []
 
     def guess_matches(self):
         '''Randomize guesses for perfect couples'''
-        # random_matches = []
-        # temp_contestants = self.contestants.copy()
         
-        # for i in range(len(self.contestants)//2):                                                  # Run loop 8 times to account for 8 pairs (given 16 contestants)
-        #     random_pair_list = random.choices(temp_contestants,k=2)         # Get two random names from list
-            
-        #     while random_pair_list[0] == random_pair_list[1]:               # Establish while loop to handle if two names are the same person
-        #         random_pair_list = random.choices(temp_contestants,k=2)
-            
-        #     while random_pair_list in self.bad_matches:
-        #         random_pair_list = random.choices(temp_contestants,k=2)
-            
-        #     random_set = set((random_pair_list[0],random_pair_list[1]) )       # Create a tuple to hold match
-        #     random_matches.append(random_set)                             # Add match tuple to list of matches
-            
-        #     temp_contestants.remove(random_pair_list[0])                    # Remove the contestants that have been matched
-        #     temp_contestants.remove(random_pair_list[1])
-
-        # self.guess = random_matches
+        # create instance of Matches class
         self.guess = Matches(self.contestants,self.bad_matches)
+
+        # return .matches attribute which contains the list of generated matches
         return self.guess.matches
-    
+
     def analyze_matches(self):
         '''Return number of correct perfect couples'''
+        
+        # initialize count starting at the number of matches already found
         self.count = len(self.matches_found)
+
+        # check all matches within generated match list
         for match in self.guess.matches:
+
+            # if the match is in the predetermined matches, increment the count
             if match in self.game.matches: self.count += 1
 
+        # if no matches are found, append all matches to the bad matches list
         if self.count == len(self.matches_found):
             for match in self.guess.matches:
                 self.bad_matches.append(match)
 
+        # return how many couples are correct
         return f'{self.count} couples are correct!'
 
     def truth_booth(self):
@@ -100,42 +145,88 @@ class Simulation:
         Random couple is sent to "truth booth"
         Returns string indicating if match is correct
         '''
+        # send a random couple to the truth booth
         self.random_couple = random.choice(self.guess.matches)
+        
+        # if this couple is a perfect match...
         if self.random_couple in self.game.matches:
+            
+            # remove contestants from remaining contestants list
             for contestant in self.random_couple:
                 self.contestants.remove(contestant)
 
+            # append the pair to the confirmed matches list
             self.matches_found.append(self.random_couple)
+
+            # return that the pair is a perfect match
             return f'{list(self.random_couple)[0]} and {list(self.random_couple)[1]} are a perfect match!'
-        else: 
+        
+        # if this couple is a bad match...
+        else:
+            
+            # add pair to bad matches list
             if self.random_couple not in self.bad_matches: self.bad_matches.append(self.random_couple)
+            
+            # return that the pair is not a perfect match
             return f'{list(self.random_couple)[0]} and {list(self.random_couple)[1]} are NOT a match!'
 
 def start_sim():
-    global names
+    'This function is used to run the simulation'
+
+    # start with a list of names to use in simulation (this is just an example list of 16 names; the game will work with any names)
     names = ['Sherri','Gunther','Clinton','Karl','Douglas','Suzanne','Jules','Jerold','Raymond','Annette','Alina','Gabriela','Maria','Placida','Mariano','Vincent']
+
+    # start an instance of the simulation
     sim = Simulation(names)
-    # print(f'Contestants: {sim.game.contestants}')
-    print(f'Contestants: {names}')
+
+    # print a list of all contestants at the start of the game
+    print(f'The contestants for this season are: {sim.contestants[0]}, {sim.contestants[1]}, {sim.contestants[2]}, {sim.contestants[3]}, {sim.contestants[4]}, {sim.contestants[5]}, {sim.contestants[6]}, {sim.contestants[7]}, {sim.contestants[8]}, {sim.contestants[9]}, {sim.contestants[10]}, {sim.contestants[11]}, {sim.contestants[12]}, {sim.contestants[13]}, {sim.contestants[14]}, and {sim.contestants[15]}')
+
+    # initialize while loop to allow game to continue until all matches are found
+    # contains the actions that happen each week
+    # 1) contestants get with who they think is their perfect match
+    # 2) # of perfect matches is announced
+    # 3) one couple is sent to truth booth
+
     finish = False
-    week = 0
+    week = 0        # week counter
+
     while not finish:
+        # increment week counter
         week += 1
+
+        # add "Week #" header for readability
         print(f'\nWeek {week}\n----------------------')
+
+        # contestants form pairs
         sim.guess_matches()
+
+        # number of perfect matches is announced
         analyze = sim.analyze_matches()
         print(analyze)
-        if sim.count == len(sim.game.matches):
-            print('All matches found!')
-            print(f'Perfect matches: {sim.game.matches}')
-            return
-        if sim.count != len(sim.matches_found):
-            truth = sim.truth_booth()
-            print(f'{list(sim.random_couple)[0]} and {list(sim.random_couple)[1]} have been sent to the truth booth!')
-            print(truth)
-        else: print('No new correct matches')
-        if sim.matches_found != []:
-            print(f'Perfect matches found: {sim.matches_found}')
-            
 
+        # if this number is equal to the amount of perfect matches needed, the game is over
+        if sim.count == len(sim.game.matches):
+            print('All matches found!')                     # announce the game is over
+            print(f'Perfect matches: {sim.game.matches}')   # announce what the perfect matches are
+
+            return                                          # end function
+
+        # if the number of correct couples is not the same as the number of correct couples already found (in this case there would be no new matches)
+        if sim.count != len(sim.matches_found):
+
+            # send one couple to the truth booth to see if they are a perfect match
+            truth = sim.truth_booth()
+
+            # announce who will be sent to the truth booth and if they are a perfect match
+            print(f'\n{list(sim.random_couple)[0]} and {list(sim.random_couple)[1]} have been sent to the truth booth!')
+            print(truth)
+
+        # if this is the case, announce there are no new correct matches
+        else: print('No new correct matches')
+
+        # at the end of each week, announce how many matches have been confirmed so far
+        print(f'\nConfirmed matches: {len(sim.matches_found)}')
+
+# start simulation
 start_sim()
